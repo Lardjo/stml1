091106c02f5f6a61a34451fbe56edb3c74a1f4b4
Konstantin <knikitanov@me.com>
2013-03-16 16:30:06 +0400

    Bumped version number to 1.1.0

--- steam_stats/__init__.py
+++ steam_stats/__init__.py
@@ -3,7 +3,7 @@
 
 __name__ = "Steam Stats"
 __description__ = "User statistics for Steam"
-__version__ = "1.0.2"
+__version__ = "1.1.0"
 __author__ = "Konstantin N."
 __license__ = "BSD"
 __url__ = "http://github.com/Lardjo/steam-stats"
