9cfc0b30bef70e768010c5d16d97bab94bf05d3e
Konstantin <knikitanov@me.com>
2013-03-19 20:35:07 +0400

    add download function

--- steam_stats/download.py
+++ steam_stats/download.py
@@ -2,3 +2,30 @@
 # Steam Stats
 
 import requests
+import xml.etree.ElementTree as ET
+
+class ConnectError(Exception):
+	pass
+
+def Download(Link=None):
+
+	filename = os.path("temp.xml")
+
+	try:
+		r = requests.get(Link)
+	except:
+		raise ConnectError
+
+	if r.status_code in (404, 500, 503):
+		raise ConnectError
+	else:
+		pass
+
+	with open(filename, "wb") as code:
+		code.write(r.content)
+
+	tree = ET.parse(filename)	
+	root = tree.getroot()
+	os.remove(filename)
+
+	return root
