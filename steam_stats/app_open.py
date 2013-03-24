735b161139891a3e0715837f3d75627cd4e6bd8d
Konstantin <knikitanov@me.com>
2013-03-25 00:42:00 +0400

    work in progress

--- steam_stats/app_open.py
+++ steam_stats/app_open.py
@@ -2,30 +2,27 @@ import requests
 import re
 import ConfigParser
 
-from flask import Flask, render_template, g, session, flash, redirect, url_for
+from flask import Flask, render_template, g, session, flash, redirect
 from flask_openid import OpenID
 from pymongo import MongoClient
 
 # setup app
 config = ConfigParser.ConfigParser()
 config.read('config.ini')
-
 # setup flask
 app = Flask(__name__)
 app.config.update(
     SECRET_KEY = 'development key',
     DEBUG = True
 )
-
 # setup flask-openid
 oid = OpenID(app)
 _steam_id_re = re.compile('steamcommunity.com/openid/id/(.*?)$')
-
 STEAM_API_KEY = config.get('data', 'apikey')
-
 # connection
 connection = MongoClient()
-db = connection.stats_base\
+db = connection.stats_base
+
 
 def get_steam_userinfo(steam_id):
     """Get JSON file"""
@@ -34,25 +31,20 @@ def get_steam_userinfo(steam_id):
     jsnobj = r.json()
     return jsnobj['response']['players'][0] or {}
 
-def get_or_create(steam_id):
-    """Search in the base"""
-    rv = db.posts.find_one({"steamid": steam_id})
-    if rv is None:
-        rv = db.posts.insert({"steamid": steam_id, "nickname": "None"})
-    return rv
 
 @app.before_request
 def before_request():
+    """Before request"""
     g.user = None
-    g.name = None
     if 'user_id' in session:
         g.user = session['user_id']
-        g.name = session['name']
+
 
 @app.route('/')
 def main():
     """Flask load"""
     return render_template('index.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 @oid.loginhandler
@@ -62,22 +54,27 @@ def login():
         return redirect(oid.get_next_url())
     return oid.try_login('http://steamcommunity.com/openid')
 
+
 @oid.after_login
 def create_or_login(resp):
+    """Create or login"""
     match = _steam_id_re.search(resp.identity_url)
-    g.user = get_or_create(match.group(1))
-    steamdata = get_steam_userinfo(g.user["steamid"])
-    g.user['nickname'] = steamdata['personaname']
-    session['user_id'] = g.user['steamid']
-    session['name'] = g.user['nickname']
+    rv = db.posts.find_one({"steamid": match.group(1)})
+    if rv is None:
+        steamdata = get_steam_userinfo(match.group(1))
+        rv = {"steamid": match.group(1), "nickname": steamdata['personaname']}
+        db.posts.insert(rv)
+    g.user = rv
+    session['user_id'] = g.user
     flash('You are logged in as %s' % g.user['nickname'])
     return redirect(oid.get_next_url())
 
+
 @app.route('/logout')
 def logout():
-	"""Logout function"""
-	session.pop('user_id', None)
-	return redirect(oid.get_next_url())
+    """Logout function"""
+    session.pop('user_id', None)
+    return redirect(oid.get_next_url())
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
