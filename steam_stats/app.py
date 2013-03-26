b68ff0b176b9907127c5528d92742916fa2305c3
Konstantin <knikitanov@me.com>
2013-03-26 19:08:11 +0400

    add url dict, delete geturl.py

--- steam_stats/app.py
+++ steam_stats/app.py
@@ -27,6 +27,10 @@ db = connection.stats_base
 
 # other
 _steam_id_re = re.compile('steamcommunity.com/openid/id/(.*?)$')
+url = {"profile": "http://steamcommunity.com/id/{0}?xml=1",
+       "games": "http://steamcommunity.com/id/{0}/games?xml=1",
+       "matchid": "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?format=XML&matches_requested=1&account_id={0}&key={1}",
+       "matchinfo": "https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?format=XML&match_id={0}&key={1}"}
 
 
 def get_steam_userinfo(steam_id):
--- steam_stats/geturl.py
+++
@@ -1,7 +0,0 @@
-# Steam Stats
-# URL Lib
-
-url = {	"profile": "http://steamcommunity.com/id/{0}?xml=1",
-		"games": "http://steamcommunity.com/id/{0}/games?xml=1",
-		"matchid": "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?format=XML&matches_requested=1&account_id={0}&key={1}",
-		"matchinfo": "https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?format=XML&match_id={0}&key={1}"}
