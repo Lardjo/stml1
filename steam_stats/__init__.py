8f5aa10333a17c10e5be25b4ded1b4d674c11fb9
Konstantin <knikitanov@me.com>
2013-03-26 17:52:56 +0400

    fix init

--- steam_stats/__init__.py
+++ steam_stats/__init__.py
@@ -1,9 +1,5 @@
-# Welcome
-# __init__.py
-
-__name__ = "Steam Stats"
-__description__ = "User statistics for Steam"
-__version__ = "1.1.1"
-__author__ = "Konstantin N."
-__license__ = "BSD"
-__url__ = "http://github.com/Lardjo/steam-stats"
+__name__ = 'Steam Stats'
+__description__ = 'User statistics for Steam'
+__version__ = '1.1.1'
+__author__ = 'Konstantin'
+__url__ = 'http://github.com/Lardjo/steam-stats'
