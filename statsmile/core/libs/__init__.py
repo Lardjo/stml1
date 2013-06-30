b5b0b8233114a36821b0ae99a1dbbac1851d5b2e
Konstantin <knikitanov@me.com>
2013-06-30 18:18:26 +0400

    Add Dota 2 Library

---
+++ statsmile/core/libs/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# All right reserved 2013
+# Description: Dota 2 Library (inc. items, heroes)
+# URL: http://github.com/Lardjo
+
+__author__ = 'Konstantin N.'
+__version__ = '0.9.9'
+
+from .dota2lib import (mode, items, heroes, cluster)
