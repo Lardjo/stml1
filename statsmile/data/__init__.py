42f351f3e941ba980067b78984e4a5a65e067b7a
Lardjo <knikitanov@me.com>
2013-10-10 18:40:57 +0400

    Import GetGames

--- statsmile/data/__init__.py
+++ statsmile/data/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from .getuser import GetUser
 from .getdota import GetDota
+from .getgames import GetGames
 from .api import APIKEY
 from .steam32 import steam64to32
 from .mongo import db
