1059c81ae52e87b9d6e6bceca93c523c1ee64614
Lardjo <knikitanov@me.com>
2013-10-28 00:40:40 +0400

    Import rename

--- statsmile/data/__init__.py
+++ statsmile/data/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 from .user import get_steam_user
 from .steam32 import steamid
-from .matchid import get_dota_matches_id
+from .matchid import GetMatchesID
 from .matches import UpdateMatches
