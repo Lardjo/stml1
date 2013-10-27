12a2ed7c97f90b211f23447b6159a68ffdf9c7f3
Lardjo <knikitanov@me.com>
2013-10-28 00:42:13 +0400

    Getting matches id

--- statsmile/data/matchid.py
+++ statsmile/data/matchid.py
@@ -1,19 +1,46 @@
 #!/usr/bin/env python3
-import requests
-import json
+import tornado.escape
 
-def get_dota_matches_id(db, steamid):
+from tornado import gen
+from tornado.httputil import url_concat
+from tornado.httpclient import AsyncHTTPClient
 
-    first = []
 
-    key = db["settings"].find_one()
-    options = {'key': key["apikey"], 'account_id': steamid}
-    r = requests.get("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/", params=options)
-    f = json.loads(r.text)
+class GetMatchesID(object):
 
-    for mid in f['result']['matches']:
-        first.append(mid['match_id'])
+    def __init__(self, db, log):
+        self.db = db
+        self.logger = log
 
-    first.sort()
+    @gen.coroutine
+    def get_matches(self, steamid):
 
-    return first
+        matches = []
+        start_time = 0
+        remaining = 1
+
+        while remaining:
+
+            key = self.db["settings"].find_one()
+            params = {'key': key['apikey'], 'account_id': steamid, 'date_max': start_time}
+            url = url_concat("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/", params)
+
+            response = yield AsyncHTTPClient().fetch(url)
+
+            pack = tornado.escape.json_decode(response.body)
+
+            for match in pack['result']['matches']:
+
+                if match['match_id'] in matches:
+                    pass
+                else:
+                    matches.append(match['match_id'])
+
+            start_time = pack['result']['matches'].pop()['start_time']
+            remaining = pack['result']['results_remaining']
+
+        else:
+            matches.sort()
+            self.db['users'].update({"steamid": steamid}, {'$set': {"matches": matches}})
+            self.logger.info("New user {} successfully added to the database. Added '{}' matches".format(steamid,
+                                                                                                         len(matches)))
