abc7370c2d0b5075e30585d8d5a04ac5834ec73b
Lardjo <knikitanov@me.com>
2013-10-28 00:40:26 +0400

    User rework

--- statsmile/data/user.py
+++ statsmile/data/user.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
-import requests
+import tornado.escape
 
-def get_steam_user(db, steamid):
-    """
-    Get user information
-    """
+from tornado import httpclient
+from tornado.httputil import url_concat
+
+def get_steam_user(db, log, steamid):
+
+    array = None
+
     key = db["settings"].find_one()
-    options = {'key': key['apikey'], 'steamids': steamid}
-    r = requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/", params=options)
-    response = r.json()
-    return response['response']['players'][0] or {}
+    params = {'key': key['apikey'], 'steamids': steamid}
+    url = url_concat("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/", params)
+
+    http_client = httpclient.HTTPClient()
+
+    try:
+        response = http_client.fetch(url)
+        array = tornado.escape.json_decode(response.body)
+    except httpclient.HTTPError as e:
+        log.info("HTTP Error:", e)
+
+    http_client.close()
+
+    return array['response']['players'][0] or {}
