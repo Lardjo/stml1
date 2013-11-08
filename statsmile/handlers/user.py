65b6357e41a739b6760b283976cedf9d1f04c31d
Lardjo <knikitanov@me.com>
2013-11-08 22:34:35 +0400

    Full matches

--- statsmile/handlers/user.py
+++ statsmile/handlers/user.py
@@ -6,8 +6,12 @@ from .base import BaseHandler
 
 class UserHandler(BaseHandler):
     def get(self, sid=None):
+        last = []
         session = self.application.db['users'].find_one({"_id": ObjectId(self.current_user)})
+        for it in reversed(session["matches"][-5:]):
+            match = self.application.db["matches"].find_one({"match_id": it})
+            last.append(match)
         if session:
-            self.render("profile.html", session=session)
+            self.render("profile.html", session=session, matches=last)
         else:
             self.render("404.html")
