2c577b9c74e65030359e28f64d6fdfb21a8f582b
Lardjo <knikitanov@me.com>
2013-11-10 22:29:06 +0400

    Added MatchesHandler

---
+++ statsmile/handlers/matches.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import tornado.web
+
+from .base import BaseHandler
+from bson import ObjectId
+
+
+class MatchesHandler(BaseHandler):
+    def get(self):
+        session = self.application.db['users'].find_one({"_id": ObjectId(self.current_user)})
+        matches = self.application.db["matches"].find().sort("match_id", -1).limit(50)
+        self.render("matches.html", session=session, matches=matches)
+
