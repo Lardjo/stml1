dad7aed732179190c2b52270de2855372f65f2fe
Lardjo <knikitanov@me.com>
2013-11-10 23:02:45 +0400

    Limit 20 on page for preview last matches

--- statsmile/handlers/matches.py
+++ statsmile/handlers/matches.py
@@ -9,6 +9,6 @@ from bson import ObjectId
 class MatchesHandler(BaseHandler):
     def get(self):
         session = self.application.db['users'].find_one({"_id": ObjectId(self.current_user)})
-        matches = self.application.db["matches"].find().sort("match_id", -1).limit(50)
+        matches = self.application.db["matches"].find().sort("match_id", -1).limit(20)
         self.render("matches.html", session=session, matches=matches)
 
