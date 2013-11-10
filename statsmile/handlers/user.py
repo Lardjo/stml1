ce3e284347e1d7cc3694fc37a811e103b670610b
Lardjo <knikitanov@me.com>
2013-11-10 22:39:21 +0400

    UserHandler rework

--- statsmile/handlers/user.py
+++ statsmile/handlers/user.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3
 
-from bson import ObjectId
 from .base import BaseHandler
 
 
 class UserHandler(BaseHandler):
     def get(self, sid=None):
-        last = []
-        session = self.application.db['users'].find_one({"_id": ObjectId(self.current_user)})
-        for it in reversed(session["matches"][-5:]):
-            match = self.application.db["matches"].find_one({"match_id": it})
-            last.append(match)
-        if session:
-            self.render("profile.html", session=session, matches=last)
-        else:
-            self.render("404.html")
+        user = self.application.db["users"].find_one({"steamid": sid})
+        self.render("profile.html", user=user)
