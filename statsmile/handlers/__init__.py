50d690b7e26b1f7a633e14012205f521e03a98e1
Lardjo <knikitanov@me.com>
2013-11-10 22:39:45 +0400

    Added two new handlers

--- statsmile/handlers/__init__.py
+++ statsmile/handlers/__init__.py
@@ -5,3 +5,5 @@ from .auth import AuthHandler
 from .user import UserHandler
 from .about import AboutHandler
 from .logout import LogoutHandler
+from .matches import MatchesHandler
+from .players import PlayersHandler
