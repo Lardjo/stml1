218915bd71c0ac0fa84d5ba85fcb85137a4033c8
Lardjo <knikitanov@me.com>
2013-11-15 18:34:21 +0400

    User data update

--- statsmile/data/update.py
+++ statsmile/data/update.py
@@ -13,16 +13,21 @@ def update_matches_id(db, log, steamid):
     update = []
     key = db["settings"].find_one()
     params = {"key": key["apikey"], "account_id": steamid}
+    params2 = {"key": key["apikey"], "steamids": steamid}
     url = url_concat("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/", params)
+    url2 = url_concat("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/", params2)
 
-    response = yield AsyncHTTPClient().fetch(url)
-    if response.error:
+    http_client = AsyncHTTPClient()
+    response, response2 = yield [http_client.fetch(url),
+                                 http_client.fetch(url2)]
+    if response.error or response2.error:
         log.info("User '{}' has not updated. Remote server not respond".format(steamid))
         db['users'].update({"steamid": steamid}, {'$set': {"next_update": datetime.now() + timedelta(minutes=15),
                                                            "last_update": datetime.now()}})
         return
     else:
         array = json_decode(response.body)
+        user = json_decode(response2.body)['response']['players'][0]
 
     for mid in array['result']['matches']:
         last.append(mid['match_id'])
@@ -31,16 +36,11 @@ def update_matches_id(db, log, steamid):
     slices = db['users'].find_one({"steamid": steamid}, {"matches": {"$slice": -100}})
 
     for key in last:
-        if key in slices['matches']:
-            pass
-        else:
+        if not key in slices['matches']:
             update.append(key)
 
-    if not update:
-        log.info("User '{}' has been updated. New matches not found".format(steamid))
-    else:
-        db['users'].update({"steamid": steamid}, {'$push': {"matches": {"$each": update}}})
-        log.info("User '{}' has been updated. Added '{}' matches".format(steamid, len(update)))
-
+    db['users'].update({"steamid": steamid}, {'$set': user})
+    db['users'].update({"steamid": steamid}, {'$push': {"matches": {"$each": update}}})
     db['users'].update({"steamid": steamid}, {'$set': {"next_update": datetime.now() + timedelta(minutes=15),
                                                        "last_update": datetime.now()}})
+    log.info("User '{}' has been updated. Added '{}' matches".format(steamid, len(update)))
