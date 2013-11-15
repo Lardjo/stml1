27ad23f7173769cdc7e44c8e18542e23d4db8870
Lardjo <knikitanov@me.com>
2013-11-15 22:56:02 +0400

    Update steam profile

--- statsmile/data/update.py
+++ statsmile/data/update.py
@@ -16,19 +16,16 @@ def update_matches_id(db, log, steamid):
     params2 = {"key": key["apikey"], "steamids": steamid}
     url = url_concat("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/", params)
     url2 = url_concat("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/", params2)
-
-    http_client = AsyncHTTPClient()
-    response, response2 = yield [http_client.fetch(url),
-                                 http_client.fetch(url2)]
+    response = yield AsyncHTTPClient().fetch(url)
+    response2 = yield AsyncHTTPClient().fetch(url2)
     if response.error or response2.error:
-        log.info("User '{}' has not updated. Remote server not respond".format(steamid))
+        log.warning("User '{}' has not updated. Remote server not respond".format(steamid))
         db['users'].update({"steamid": steamid}, {'$set': {"next_update": datetime.now() + timedelta(minutes=15),
                                                            "last_update": datetime.now()}})
         return
     else:
         array = json_decode(response.body)
         user = json_decode(response2.body)['response']['players'][0]
-
     for mid in array['result']['matches']:
         last.append(mid['match_id'])
 
@@ -38,7 +35,6 @@ def update_matches_id(db, log, steamid):
     for key in last:
         if not key in slices['matches']:
             update.append(key)
-
     db['users'].update({"steamid": steamid}, {'$set': user})
     db['users'].update({"steamid": steamid}, {'$push': {"matches": {"$each": update}}})
     db['users'].update({"steamid": steamid}, {'$set': {"next_update": datetime.now() + timedelta(minutes=15),
