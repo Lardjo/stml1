19c45838f6f955d5e15fee26cbb67052f0a10894
Lardjo <knikitanov@me.com>
2013-11-20 12:41:17 +0400

    fix

--- statsmile/handlers/matches.py
+++ statsmile/handlers/matches.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-import tornado.web
-
 from .base import BaseHandler
 from bson import ObjectId
 
@@ -11,4 +9,3 @@ class MatchesHandler(BaseHandler):
         session = self.application.db['users'].find_one({"_id": ObjectId(self.current_user)})
         matches = self.application.db["matches"].find().sort("match_id", -1).limit(20)
         self.render("matches.html", session=session, matches=matches)
-
