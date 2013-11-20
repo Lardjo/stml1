a0914fd1d0120d5691f4bf2e8acff03ee2a36ce9
Lardjo <knikitanov@me.com>
2013-11-20 12:29:41 +0400

    Session

--- statsmile/handlers/match.py
+++ statsmile/handlers/match.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+from bson import ObjectId
+
 from .base import BaseHandler
 
 
 class MatchHandler(BaseHandler):
     def get(self, match):
+        session = self.application.db["users"].find_one({"_id": ObjectId(self.current_user)})
         match = self.application.db["matches"].find_one({"match_id": int(match)})
-        self.render("match.html", match=match)
+        self.render("match.html", session=session, match=match)
