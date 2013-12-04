03c1f5ce602c6b6ee81171887b9707f158fe4a02
Lardjo <knikitanov@me.com>
2013-12-04 19:17:09 +0400

    Some changes in index page. FAQ added

--- statsmile/handlers/page.py
+++ statsmile/handlers/page.py
@@ -7,14 +7,14 @@ from bson import ObjectId
 class PageHandler(BaseHandler):
     def get(self, source):
         session = self.application.db["users"].find_one({"_id": ObjectId(self.current_user)})
-        if source not in ('about', 'privacy', 'start', 'status'):
+        if source not in ('about', 'privacy', 'faq', 'status'):
             return self.send_error(404)
         elif source == "about":
             self.render("page.html", title="About", session=session)
         elif source == "privacy":
             self.render("page.html", title="Privacy Policy", session=session)
-        elif source == "start":
-            self.render("page.html", title="Getting started", session=session)
+        elif source == "faq":
+            self.render("page.html", title="FAQ", session=session)
         elif source == "status":
             status_dota = self.application.db["status"].find_one({"status": "api_dota"})
             status_steam = self.application.db["status"].find_one({"status": "api_steam"})
