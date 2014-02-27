df95af15b4537ca6a0daaa711be135b1bcd5d42f
Lardjo <knikitanov@me.com>
2014-02-27 12:17:52 +0400

    Init commit for 2.0

--- README.md
+++
@@ -1,3 +0,0 @@
-**Statsmile** version 1.2.4.1
-
-Only for more detailed acquaintance.
--- statsmile/common/libs/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# Dota 2 Lib. Version 1.0.8
-from .clusters import cluster
-from .modes import mode
-from .heroes import heroes
-from .items import items
-from .heroes_info import heroes_info
