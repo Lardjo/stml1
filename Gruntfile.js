a20d919f551d9e5cacb1ad6da8accabdedefeabe
Lardjo <knikitanov@me.com>
2014-03-05 21:16:49 +0400

    Added all images, create lib for dota 2, match page table (beta)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,71 +1,90 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    
-    sass: {
-      dist: {
-        options: {
-          style: 'compact'
-        },
-        files: {
-          'statsmile/web/static/css/app.css': 'statsmile/web/frontend/scss/app.scss',
-          'statsmile/web/static/css/bootstrap.css': 'statsmile/web/frontend/scss/bootstrap.scss'
-        }
-      }
-    },
-    
-    emberTemplates: {
-      compile: {
-        options: {
-          templateBasePath: /statsmile\/web\/frontend\/js\/app\/templates\//
-        },
-        files: {
-          'statsmile/web/static/js/templates.js': 'statsmile/web/frontend/js/app/templates/**/*.hbs'
-        }
-      }
-    },
-    
-    concat: {
-      libs: {
-        src: [
-          'statsmile/web/frontend/js/libs/jquery-2.1.0.min.js',
-          'statsmile/web/frontend/js/libs/handlebars-1.1.2.js',
-          'statsmile/web/frontend/js/libs/ember-1.4.0.js',
-          'statsmile/web/frontend/js/libs/ember-data.js',
-          'statsmile/web/frontend/js/libs/bootstrap.min.js',
-          'statsmile/web/frontend/js/libs/showdown.js',
-          'statsmile/web/frontend/js/libs/moment.min.js'
-        ],
-        dest: 'statsmile/web/static/js/libs.js'
-      },
-      app: {
-        src: 'statsmile/web/frontend/js/app/**/*.js',
-        dest: 'statsmile/web/static/js/app.js'
-      }
-    },
-    
-    watch: {
-      sass: {
-        files: ['statsmile/web/frontend/scss/*.scss', 'statsmile/web/frontend/scss/app/*.scss', 'statsmile/web/frontend/scss/bootstrap/*.scss'],
-        tasks: ['sass']
-      }, 
-      emberTemplates: {
-        files: 'statsmile/web/frontend/js/app/templates/**/*.hbs',
-        tasks: ['emberTemplates']
-      }, 
-      concat: {
-        files: ['statsmile/web/frontend/js/**/*.js', 'statsmile/web/frontend/js/app/**/*.js', '!statsmile/web/static/js/app.js', '!statsmile/web/static/js/libs.js', '!statsmile/web/static/js/templates.js'],
-        tasks: ['concat']
-      }
-    }
 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'compact'
+                },
+                files: {
+                    'statsmile/web/static/css/app.css': 'statsmile/web/frontend/scss/app.scss'
+                }
+            }
+        },
+
+        emberTemplates: {
+            compile: {
+                options: {
+                    templateBasePath: /statsmile\/web\/frontend\/js\/app\/templates\//
+                },
+                files: {
+                    'statsmile/web/static/js/templates.js': 'statsmile/web/frontend/js/app/templates/**/*.hbs'
+                }
+            }
+        },
+
+        concat: {
+            libs: {
+                src: [
+                    'statsmile/web/frontend/js/libs/jquery-2.1.0.min.js',
+                    'statsmile/web/frontend/js/libs/bootstrap.min.js',
+                    'statsmile/web/frontend/js/libs/handlebars-v1.3.0.js',
+                    'statsmile/web/frontend/js/libs/ember-1.4.0.js',
+                    'statsmile/web/frontend/js/libs/showdown.js',
+                    'statsmile/web/frontend/js/libs/moment.min.js',
+                    'statsmile/web/frontend/js/libs/dota.js'
+                ],
+                dest: 'statsmile/web/static/js/libs.js'
+            },
+            app: {
+                src: 'statsmile/web/frontend/js/app/**/*.js',
+                dest: 'statsmile/web/static/js/app.js'
+            }
+        },
+
+        /*uglify: {
+            options: {
+                mangle: false
+            },
+            my_target: {
+                files: {
+                    'statsmile/web/static/js/libs.min.js': ['statsmile/web/static/js/libs.js']
+                }
+            }
+        },*/
+
+        watch: {
+            sass: {
+                files: [
+                    'statsmile/web/frontend/scss/*.scss',
+                    'statsmile/web/frontend/scss/app/*.scss',
+                ],
+                tasks: ['sass']
+            },
+            emberTemplates: {
+                files: 'statsmile/web/frontend/js/app/templates/**/*.hbs',
+                tasks: ['emberTemplates']
+            },
+            concat: {
+                files: [
+                    'statsmile/web/frontend/js/**/*.js',
+                    '!statsmile/web/static/js/app.js',
+                    '!statsmile/web/static/js/libs.js',
+                    '!statsmile/web/static/js/templates.js'
+                ],
+                tasks: ['concat']
+            }
+        }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-ember-templates');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    /*grunt.loadNpmTasks('grunt-contrib-uglify');*/
+    grunt.loadNpmTasks('grunt-ember-templates');
 
-  // Default task(s).
-  grunt.registerTask('default', ['sass', 'concat', 'emberTemplates']);
+    // Default task(s).
+    grunt.registerTask('default', ['sass', 'concat', 'emberTemplates']);
 };
--- statsmile/web/frontend/js/app/helpers/helpers.js
+++ statsmile/web/frontend/js/app/helpers/helpers.js
@@ -23,18 +23,6 @@ Ember.Handlebars.helper('ranked', function(lobby_type) {
 });
 
 Ember.Handlebars.helper('lobby', function(lobby_type) {
-
-    var lobbies = {
-            0: {'name' : 'Public'},
-            1: {'name' : 'Practice'},
-            2: {'name' : 'Tournament'},
-            3: {'name' : 'Tutorial'},
-            4: {'name' : 'Co-op With Bots'},
-            5: {'name' : 'Team match'},
-            6: {'name' : 'Solo Queue'},
-            7: {'name' : 'Ranked'}
-    };
-
     if (lobby_type in lobbies) {
         return lobbies[lobby_type]['name'];
     } else {
@@ -43,76 +31,74 @@ Ember.Handlebars.helper('lobby', function(lobby_type) {
 });
 
 Ember.Handlebars.helper('game-mode', function(game_mode) {
-
-    var mode = {
-        0: {'name': 'Unknown'},
-        1: {'name': 'All Pick'},
-        2: {'name': 'Captains Mode'},
-        3: {'name': 'Random Draft'},
-        4: {'name': 'Single Draft'},
-        5: {'name': 'All Random'},
-        7: {'name': 'Diretide'},
-        9: {'name': 'Greeviling'},
-        12: {'name': 'Least Played'},
-        13: {'name': 'Limited Hero'},
-        14: {'name': 'Compendium Matchmaking'},
-        15: {'name': 'The Year Beast'}, /*Wraith-Night*/
-        16: {'name': 'Captains Draft'},
-        17: {'name': 'Balanced Draft'},
-        18: {'name': 'Ability Draft'}
-    };
-
-    if (game_mode in mode) {
-        return mode[game_mode]['name'];
+    if (game_mode in game_modes) {
+        return game_modes[game_mode]['name'];
     } else {
         return 'Unknown';
     }
 });
 
 Ember.Handlebars.helper('cluster', function(cluster_id) {
-
-    var cluster = {
-        0: {'name': 'Unknown'},
-        111: {'name': 'US West'},
-        112: {'name': 'US West'},
-        114: {'name': 'US West'},
-        121: {'name': 'US East'},
-        122: {'name': 'US East'},
-        123: {'name': 'US East'},
-        124: {'name': 'US East'},
-        131: {'name': 'Europe'},
-        132: {'name': 'Europe'},
-        133: {'name': 'Europe'},
-        134: {'name': 'Europe'},
-        135: {'name': 'Europe'},
-        136: {'name': 'Europe'},
-        142: {'name': 'Hong Kong'},
-        143: {'name': 'Hong Kong'},
-        151: {'name': 'China'},
-        152: {'name': 'China'},
-        153: {'name': 'China'},
-        161: {'name': 'Japan'},
-        163: {'name': 'China'},
-        171: {'name': 'Australia'},
-        191: {'name': 'Europe East'},
-        181: {'name': 'Russia'},
-        182: {'name': 'Russia'},
-        183: {'name': 'Russia'},
-        184: {'name': 'Russia'},
-        185: {'name': 'Russia'},
-        186: {'name': 'Russia'},
-        200: {'name': 'South America'},
-        204: {'name': 'South America'},
-        211: {'name': 'South Africa'},
-        212: {'name': 'South Africa'},
-        213: {'name': 'South Africa'},
-        221: {'name': 'China'},
-        222: {'name': 'China'}
-    };
-
-    if (cluster_id in cluster) {
-        return cluster[cluster_id]['name'];
+    if (cluster_id in clusters) {
+        return clusters[cluster_id]['name'];
     } else {
         return 'Unknown';
     }
+});
+
+Ember.Handlebars.helper('item', function(item_id) {
+    if (item_id in items) {
+        return new Ember.Handlebars.SafeString(
+            "<img src='/static/images/items/" + items[item_id]['avatar'] + ".png' />"
+        );
+    } else {
+        return new Ember.Handlebars.SafeString(
+            "<img src='/static/images/items/default.png' />"
+        );
+    }
+});
+
+Ember.Handlebars.helper('hero', function(hero_id) {
+    if (hero_id in heroes) {
+        return new Ember.Handlebars.SafeString(
+            "<img src='/static/images/heroes/" + heroes[hero_id]['avatar'] + ".png' />"
+        );
+    } else {
+        return new Ember.Handlebars.SafeString(
+            "<img src='/static/images/heroes/default.png' />"
+        );
+    }
+});
+
+Ember.Handlebars.helper('progress-kills', function(kills, deaths, assists) {
+    var progress = 0;
+    var sum = kills + deaths + assists;
+    if (sum > 0) {
+        progress = (kills * 100 / sum).toFixed(4);
+    }
+    return new Handlebars.SafeString(
+      "<div class='progress-bar progress-bar-success' style='width:" + progress + "%'>" + kills + "</div>"
+    );
+});
+
+Ember.Handlebars.helper('progress-deaths', function(kills, deaths, assists) {
+    var progress = 0;
+    var sum = kills + deaths + assists;
+    if (sum > 0) {
+        progress = (deaths * 100 / sum).toFixed(4);
+    }
+    return new Handlebars.SafeString(
+        "<div class='progress-bar progress-bar-danger' style='width:" + progress + "%'>" + deaths + "</div>"
+    );
+});
+
+Ember.Handlebars.helper('progress-assists', function(kills, deaths, assists) {
+    var progress = 0;
+    var sum = kills + deaths + assists;
+    if (sum > 0) {
+        progress = (assists * 100 / sum).toFixed(4);
+    }
+    return new Handlebars.SafeString(
+        "<div class='progress-bar progress-bar-warning' style='width:" + progress + "%'>" + assists + "</div>"
+    );
 });
