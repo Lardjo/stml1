abe6cbc369b03ca6a229deda5cf9c366fa02796f
Lardjo <knikitanov@me.com>
2014-03-18 19:22:15 +0400

    New init

--- statsmile/__init__.py
+++ statsmile/__init__.py
@@ -2,56 +2,77 @@
 
 import os
 import logging
-import motor
 
 from datetime import datetime, timedelta
 from functools import partial
 
-from tornado.gen import coroutine
-from tornado.web import Application, asynchronous
-from tornado.ioloop import IOLoop
-
 from motor import Op
-from pymongo import ASCENDING, DESCENDING
-from pymongo.errors import ConnectionFailure
+from pymongo import DESCENDING
+
+from tornado.gen import coroutine
+from tornado.ioloop import IOLoop
+from tornado.web import Application
 
 from statsmile import handlers
-from statsmile.common import get_secret
+from statsmile.dbase import dbconn, ensure_index
+from statsmile.common import get_secret, get_matches
+from statsmile.tasks import initiate
 
 
 class Statsmile(Application):
 
+    @coroutine
+    def matches_upd(self, _seq_id):
+        """Matches updater
+
+        Matches Tasks
+        """
+        logging.debug('MATCH SEQ NUM: | %s | Start update...' % _seq_id)
+
+        try:
+            yield get_matches(self.db, _seq_id)
+        except Exception as e:
+            logging.warning('Error: %s' % e)
+
+        logging.debug('MATCH SEQ NUM: | %s | Success!' % _seq_id)
+
+        new_array = yield Op(self.db.server.find_one, {'key': 'last_match_update'}, limit=1)
+
+        IOLoop.instance().add_timeout((datetime.now() + timedelta(seconds=1)).timestamp(),
+                                      partial(self.matches_upd, new_array['value']))
+
     def __init__(self):
 
         handlers_list = [
-            (r'/', handlers.MainHandler),
-            (r'/ws', handlers.WebSocketHandler),
-            (r'/status', handlers.StatusHandler),
-            (r'/matches', handlers.MatchesHandler),
-            (r'/matches/(.*)', handlers.MatchHandler)
+            (r'/', handlers.MainHandler)
         ]
 
-        try:
-            client = motor.MotorClient('localhost', 27017).open_sync()
-            self.db = client['Statsmile']
-            self.db_sync = client.sync_client()['Statsmile']
-        except ConnectionFailure:
-            logging.error('Could not connect to database. Exit.')
-            exit(4)
+        # Create db connection
+        self.db, self.db_sync = dbconn.db_conn()
 
-        self.db['server'].ensure_index('key', unique=True)
+        # Check the index
+        ensure_index.create_index(self.db)
 
+        # Set settings
         settings = {
             'cookie_secret': get_secret.get_cookies(self.db_sync, 'cookie_secret'),
             'gzip': True,
             'debug': True,
             'login_url': '/auth/login',
+            'last_match_id': self.db_sync.server.find_one({'key': 'last_match_update'})['value'],
             'template_path': os.path.join(os.path.dirname(__file__), 'web', 'templates'),
             'static_path': os.path.join(os.path.dirname(__file__), 'web', 'static'),
         }
 
+        # Logging
         if settings['debug']:
             logging.getLogger().setLevel(logging.DEBUG)
+
+        # Initialize background update of matches
+        logging.info('Initialize background update...')
+
+        IOLoop.instance().add_timeout((datetime.now() + timedelta(seconds=1)).timestamp(),
+                                      partial(self.matches_upd, settings['last_match_id']))
 
         super().__init__(handlers_list, **settings)
 
