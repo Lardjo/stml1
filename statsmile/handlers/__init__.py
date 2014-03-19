b44f1bffdb3e18e3e3777cfd4f8f1ca6a34f7b41
Lardjo <knikitanov@me.com>
2014-03-19 15:40:52 +0400

    Added auth handlers

--- statsmile/handlers/__init__.py
+++ statsmile/handlers/__init__.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
 
 from .main import MainHandler
-from .matches import MatchesHandler, MatchHandler
-from .ws import WebSocketHandler
-from .status import StatusHandler
+from .auth import AuthLoginHandler, AuthLogoutHandler
