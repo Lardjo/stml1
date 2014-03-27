e918454cf1b6c54e1330843fe9a2cbdb4956173d
Lardjo <knikitanov@me.com>
2014-03-27 17:30:21 +0400

    Ensure index fixed

--- statsmile/dbase/ensure_index.py
+++ statsmile/dbase/ensure_index.py
@@ -13,9 +13,16 @@ def create_index(db):
     logging.info('Scan indexes of database...')
 
     db.server.ensure_index('key', unique=True)
+
+    # Matches
     db.matches.ensure_index('start_time', DESCENDING)
     db.matches.ensure_index('match_id', DESCENDING)
     db.matches.ensure_index('players.account_id', DESCENDING)
     db.matches.ensure_index('unregistered', sparse=True)
     db.matches.ensure_index('game_mode', ASCENDING)
-    db.users.ensure_index('win_rate', DESCENDING)
+
+    # Users
+    db.users.ensure_index('winrate', DESCENDING)
+
+    # Favorites
+    db.favorites.ensure_index('id', DESCENDING)
