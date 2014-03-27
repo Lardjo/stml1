73fe2db73ca12bcbaeed624d8d1e46c7c79fdaea
Lardjo <knikitanov@me.com>
2014-03-27 17:21:31 +0400

    Auth handler fix

--- statsmile/api/auth.py
+++ statsmile/api/auth.py
@@ -3,9 +3,8 @@
 from datetime import datetime
 
 from motor import Op
-from tornado import gen
 from tornado.auth import OpenIdMixin
-from tornado.gen import engine
+from tornado.gen import engine, coroutine
 from tornado.escape import json_encode
 from tornado.web import asynchronous, HTTPError
 
@@ -14,42 +13,50 @@ from .base import BaseHandler
 
 
 class AuthLoginHandler(BaseHandler, OpenIdMixin):
+    """Auth Login Handler
 
-    _OPENID_ENDPOINT = "http://steamcommunity.com/openid/login"
+    Main Authentication handler. Path: /auth/login
+    """
+    _OPENID_ENDPOINT = 'http://steamcommunity.com/openid/login'
 
     @asynchronous
-    @gen.coroutine
+    @coroutine
     def get(self):
 
-        if self.get_argument("openid.mode", None):
+        if self.get_argument('openid.mode', None):
+
             user = yield self.get_authenticated_user()
 
             if not user:
-                raise HTTPError(500, "Steam Auth Failed")
+                raise HTTPError(500, 'Steam Auth Failed')
 
-            rv = yield Op(self.db.users.find_one, {'steam_id': user['claimed_id'][-17:]})
-            temp = self.upd_session()
+            rv = yield Op(self.db.users.find_one, {'claimed_id': user['claimed_id'][-17:]})
+            sess = self.upd_session()
 
             if not rv:
-                userid = yield get_user(self.settings['api_key'], user['claimed_id'][-17:])
-                self.db.users.insert(userid)
-                temp['user_id'] = userid['_id']
-                temp['signed_in'] = datetime.now()
-                self.db.sessions.insert(temp)
+                user = yield get_user(self.settings['api_key'], user['claimed_id'][-17:])
+                self.db.users.insert(user)
+                sess['user'] = user['_id']
+                sess['signed_in'] = datetime.now()
+                self.db.sessions.insert(sess)
 
             else:
-                temp['user_id'] = rv['_id']
-                temp['signed_in'] = datetime.now()
-                self.db.sessions.insert(temp)
+                sess['user'] = rv['_id']
+                sess['signed_in'] = datetime.now()
+                self.db.sessions.insert(sess)
 
-            self.set_secure_cookie('user_session', json_encode(str(temp['_id'])))
+            self.set_secure_cookie('user_session', json_encode(str(sess['_id'])))
             self.redirect(self.get_argument('next', '/'))
-            return
+
         else:
-            yield self.authenticate_redirect()
+            self.authenticate_redirect()
 
 
 class AuthLogoutHandler(BaseHandler):
+    """Auth Logout Handler
+
+    Re-authentication Handler. Path: /auth/logout
+    """
     def get(self):
         self.db.sessions.remove({'_id': self.current_user['_id']})
         self.clear_cookie('user_session')
